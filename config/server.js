fa6bee6c2368fc0b1c56c9e0943195101a0e234a
leenawon <nawon5154@gmail.com>
2021-03-31 18:27:34 +0900

    initial Strapi DB

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd0789fc46b371c39420061d2cc4b8709'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '39956966-1eb3-4ba2-93b9-881b4a7c24b5'
+};
